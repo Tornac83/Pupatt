822640eea841a6eebdafb28c35cbabf5741705be
towbes <hank.wyatt@gmail.com>
2020-02-19 14:48:02 +0900

    Added clearall function
    Added /pupatt clear  to clear all attachments

--- pupatt.lua
+++ pupatt.lua
@@ -74,25 +74,6 @@ ashita.register_event('incoming_packet', function(id, size, packet)
 			  equippedOffset = equippedOffset + 1;
 			end
 		end
---				CurAttHead		= string.format("0x0%X" , struct.unpack('B', packet, 0x08 + 1));
---				CurAttBody 		= string.format("0x%X" ,struct.unpack('B', packet, 0x09 + 1));
---				CurAttOne 		= struct.unpack('B', packet, 0x0A + 1);
---				CurAttTwo 		= struct.unpack('B', packet, 0x0B + 1);
---				CurAttThree 	= struct.unpack('B', packet, 0x0C + 1);
---				CurAttFour 		= struct.unpack('B', packet, 0x0D + 1);
---				CurAttFive 		= struct.unpack('B', packet, 0x0E + 1);
---				CurAttSix 		= struct.unpack('B', packet, 0x0F + 1);
---				CurAttSeven 	= struct.unpack('B', packet, 0x10 + 1);
---				CurAttEight 	= struct.unpack('B', packet, 0x11 + 1);
---				CurAttNine 		= struct.unpack('B', packet, 0x12 + 1);
---				CurAttTen 		= struct.unpack('B', packet, 0x13 + 1);
---				CurAttEleven 	= struct.unpack('B', packet, 0x14 + 1);
---				CurAttTwelve 	= struct.unpack('B', packet, 0x15 + 1);
-				--print(CurAttHead, CurAttBody, CurAttOne, CurAttTwo, CurAttThree, CurAttFour, CurAttFive, CurAttSix, CurAttSeven, CurAttEight, CurAttNine, CurAttTen, CurAttEleven, CurAttTwelve);
-				--print(tonumber("0xCE"));
-				--print(string.format(CurAttHead))
-				--print(string.len(CurAttHead))
-				--print(CurAttHead, CurAttBody)
 	end
 	return false;
 end);
@@ -104,6 +85,38 @@ function addAttachment(slot, id)
 	slots[slot] = id;
 	local attach = struct.pack('I2I2BBBBBBI2BBBBBBBBBBBBBB', 0x5302, 0x0000, id, 0x00, unequip, 0x00, 0x12, pupSub, 0x0000, slots[1],slots[2],slots[3],slots[4],slots[5],slots[6],slots[7],slots[8],slots[9],slots[10],slots[11],slots[12],slots[13],slots[14]):totable();
 	table.insert(attachmentQueue, { 0x102, attach});
+end;
+ 
+function clearAttachments() 
+	local player					= GetPlayerEntity();
+	local pet 						= GetEntity(player.PetTargetIndex);
+	local recastTimerActivate   	= ashita.ffxi.recast.get_ability_recast_by_id(205);
+	local recastTimerDeactivate   	= ashita.ffxi.recast.get_ability_recast_by_id(208);
+	local recastTimerdeusex   		= ashita.ffxi.recast.get_ability_recast_by_id(115);
+	if (recastTimerDeactivate == 0 and pet ~= nil) then
+		AshitaCore:GetChatManager():QueueCommand('/ja "Deactivate" <me>' , 1);
+	elseif(recastTimerDeactivate > 0) then
+		print('Deactivate is not ready yet please try again later.')
+		return true;
+	end
+	
+	if currentAttachments[1] ~= nil and pet == nil then
+		print ("Clearing Attachments");
+		slots = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+		for slot,id in pairs(currentAttachments) do 
+			slots[slot] = id;
+		end
+		slots[1] = 0x00;
+		slots[2] = 0x00;
+		for i,id in pairs(slots) do
+			print(string.format("Equip ID: 0x%X", id));
+		end
+		print(string.format("Unequip: 0x%X", unequip));
+		local unattach = struct.pack('I2I2BBBBBBI2BBBBBBBBBBBBBB', 0x5302, 0x0000, 0x00, 0x00, 0x01, 0x00, 0x12, pupSub, 0x0000, slots[1],slots[2],slots[3],slots[4],slots[5],slots[6],slots[7],slots[8],slots[9],slots[10],slots[11],slots[12],slots[13],slots[14]):totable();
+		table.insert(attachmentQueue, { 0x102, unattach});
+	else
+		print ("Current attachments not loaded, try zoning / equipping an attachment");
+	end
 end;
 
 ----------------------------------------------------------------------------------------------------
@@ -252,8 +265,19 @@ ashita.register_event('command', function(command, ntype)
   		return true;
   	end
 	
+    if (#args >= 2 and args[2] == 'current') then
+		currentAtt = ashita.settings.JSON:encode_pretty(currentAttachments, nil, {pretty = true, indent = "->    " });
+		print(currentAtt);
+  		return true;
+  	end
+	
     if (#args >= 2 and args[2] == 'save') then
   		save_profiles()
+  		return true;
+  	end
+	
+    if (#args >= 2 and args[2] == 'clear') then
+  		clearAttachments()
   		return true;
   	end
 	
