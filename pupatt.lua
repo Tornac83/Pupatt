f588c91be67ea4af222e398116de4a3041298939
Tornac83 <61156369+Tornac83@users.noreply.github.com>
2021-03-10 00:22:36 -0600

    small change

--- pupatt.lua
+++ pupatt.lua
@@ -136,7 +136,7 @@ function clearAttachments()
         slots[2] = 0x00;
         
 		-- Build the packet..
-		local packet = string.char(0x02, 0x53, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x12, pupSub, 0x00, 0x00)
+		local packet = string.char(0x02, 0x53, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x12, pupSub, 0x00, 0x00)
 		local packet = packet .. string.char(slots[1],slots[2],slots[3],slots[4],slots[5],slots[6],slots[7],slots[8],slots[9],slots[10],slots[11],slots[12],slots[13],slots[14]) 
 		local packet = packet .. string.rep(string.char(0x00), 0x88);
 	
